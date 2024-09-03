eeff1c95b7da47b98fb8ce6840d26f9621871759
Lukas Prochazka <prochazka.lu@gmail.com>
2024-09-03 13:38:39 +0200

    Remove nested

--- app/MDXComponent.tsx
+++ app/MDXComponent.tsx
@@ -1,19 +1,26 @@
 'use client';
 
 import { MDXProvider } from '@mdx-js/react';
+import { MDXComponents } from 'mdx/types';
+import { HTMLAttributes } from 'react';
 import Content from './message.mdx';
 
-const H1 = (props) => <h1 style={{ color: 'tomato' }} {...props} />;
-const H2 = (props) => <h2 style={{ color: 'rebeccapurple' }} {...props} />;
+const components = {
+  h3: ({ children, ...props }: HTMLAttributes<HTMLHeadElement>) => (
+    <h3 style={{ color: 'purple' }} {...props}>
+      {children}
+    </h3>
+  ),
+  h4: ({ children, ...props }: HTMLAttributes<HTMLHeadElement>) => (
+    <h4 style={{ color: 'yellow' }} {...props}>
+      {children}
+    </h4>
+  ),
+} satisfies MDXComponents;
 
-export default function Home() {
+export default function MDXComponent() {
   return (
-    <MDXProvider
-      components={{
-        h1: H1,
-        h2: H2,
-      }}
-    >
+    <MDXProvider components={components}>
       <Content />
     </MDXProvider>
   );
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,23 @@
 import { MDXComponents } from 'mdx/types';
+import { HTMLAttributes } from 'react';
 
 const components = {
-  h1: (props) => <h1 style={{ color: 'tomato' }} {...props} />,
+  h1: ({ children, ...props }: HTMLAttributes<HTMLHeadElement>) => (
+    <h1 style={{ color: 'tomato' }} {...props}>
+      {children}
+    </h1>
+  ),
+  h2: ({ children, ...props }: HTMLAttributes<HTMLHeadElement>) => (
+    <h2 style={{ color: 'blue' }} {...props}>
+      {children}
+    </h2>
+  ),
 } satisfies MDXComponents;
 
 declare global {
   type MDXProvidedComponents = typeof components;
 }
-
+// eslint-disable-next-line no-undef
 export function useMDXComponents(): MDXProvidedComponents {
   return components;
 }
